af3d1b44b6dd1e89f00bc7bbe760ead4e67b82f5
Bruno Iljazovic <bruno.iljazovic@gmail.com>
2020-12-09 14:25:31 +0100

    day 7

---
+++ src/Day07.hs
@@ -0,0 +1,46 @@
+module Day07 (main07) where
+
+import Util
+import Data.Map (Map)
+import qualified Data.Set as Set
+import qualified Data.Map.Strict as Map
+import Data.Graph.Inductive.Graph
+import Data.Graph.Inductive.Basic (grev)
+import Data.Graph.Inductive.Query (reachable)
+import Data.Graph.Inductive.PatriciaTree
+
+type Bag = String
+type IT = [(Bag, [(Int, Bag)])]
+
+parseLine :: String -> (Bag, [(Int, Bag)])
+parseLine line = (topbag, bags) where
+    (topbag : bags' : _) = splitOn " bags contain " line
+    bags = if bags' == "no other bags." then [] else map f $ splitOn ", " bags'
+    f bag' = let amount : (unwords . init -> bag) = words bag'
+              in (read amount, bag)
+
+makeGraph :: IT -> (Gr Bag Int, Map Bag Int)
+makeGraph input = (mkGraph nodes edges, indexMap)
+    where
+        allBags = Set.toList $ Set.fromList $ map fst input ++ concatMap (map snd . snd) input
+        indexMap = Map.fromList $ map (\(a, b) -> (b, a)) nodes
+        nodes = zip [1..] allBags
+        edges = concatMap makeEdges input
+        makeEdges (topbag, bags) = map (\(amount, bag) -> (indexMap Map.! topbag, indexMap Map.! bag, amount)) bags
+
+
+solveA :: (Gr Bag Int, Map Bag Int) -> Int
+solveA (graph, (Map.! "shiny gold") -> shinyGold) = (\x -> x - 1) . length . reachable shinyGold . grev $ graph
+
+solveB :: (Gr Bag Int, Map Bag Int) -> Int
+solveB (graph, (Map.! "shiny gold") -> shinyGold) = rek shinyGold
+    where
+        rek n = let (_, _, _, outs) = context graph n
+                 in sum (map (\(b, c) -> b * (rek c + 1)) outs)
+
+main07 :: IO ()
+main07 = do
+    input <- map parseLine . filter (not . emptyLine) . lines <$> readFile "res/input07"
+    let (graph, indexMap) = makeGraph input
+    print $ solveA (graph, indexMap)
+    print $ solveB (graph, indexMap)
--- src/Util.hs
+++ src/Util.hs
@@ -1,14 +1,42 @@
-module Util (module Linear.V2, module Control.Lens, module Text.Megaparsec, module Text.Megaparsec.Char, module Text.Megaparsec.Char.Lexer, module Data.Void, parseMatrix, CharMatrix, sepBy1_, module Data.List, module Data.List.Split) where
+module Util
+  ( module Linear.V2,
+    module Control.Lens,
+    module Text.Megaparsec,
+    module Text.Megaparsec.Char,
+    module Text.Megaparsec.Char.Lexer,
+    module Data.Void,
+    parseMatrix,
+    CharMatrix,
+    sepBy1_,
+    module Data.List,
+    module Data.List.Split,
+    emptyLine,
+    generateBlackAndWhiteImage,
+    generateGraph,
+    susedi,
+    genericBfs
+  )
+where
 
-import Linear.V2
-import Data.Array
+import Codec.Picture
 import Control.Lens hiding (noneOf, uncons)
+import Control.Monad (guard)
+import Data.Array
+import Data.Char (isSpace)
+import Data.Graph.Inductive.Graph
+import Data.Graph.Inductive.PatriciaTree
+import Data.List
+import Data.List.Split (splitOn)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (mapMaybe)
+import qualified Data.Sequence as Seq
+import qualified Data.Set as Set
+import Data.Void (Void)
+import Linear.V2
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer hiding (space)
-import Data.Void ( Void )
-import Data.List
-import Data.List.Split (splitOn)
 
 type CharMatrix = Array (V2 Int) Char
 
@@ -18,7 +46,59 @@ parseMatrix str =
       n = length strs
       m = length $ head strs
       lst = concat $ [[(V2 i j, ch) | (ch, j) <- zip str [0 ..]] | (str, i) <- zip strs [0 ..]]
-   in array (V2 0 0, V2 (n-1) (m-1)) lst
+   in array (V2 0 0, V2 (n -1) (m -1)) lst
 
 sepBy1_ :: MonadParsec e s m => m a -> m sep -> m [a]
 sepBy1_ p sep = (:) <$> try p <*> many (try (sep *> p))
+
+genericBfs ::
+  Ord a =>
+  (a -> Bool) -> -- goal
+  (a -> [a]) -> -- neighbors
+  a -> -- start
+  Maybe Int -- distance from start to goal
+genericBfs goal neighs start = bfs (Set.singleton start) (Seq.singleton (start, 0))
+  where
+    bfs seen queue = case queue of
+      (Seq.viewl -> (cur, dist) Seq.:< rest) ->
+        if goal cur
+          then Just dist
+          else
+            let (newSeen, newQueue) = foldr g (seen, rest) (neighs cur)
+                g neigh (seen', queue') =
+                  if Set.member neigh seen'
+                    then (seen', queue')
+                    else (Set.insert neigh seen', queue' Seq.|> (neigh, dist + 1))
+             in bfs newSeen newQueue
+      _ -> Nothing
+
+generateBlackAndWhiteImage :: String -> a -> Map (V2 Int) a -> (a -> PixelRGB8) -> IO ()
+generateBlackAndWhiteImage filename defaultValue mapa toColor = saveBmpImage filename image
+  where
+    image = ImageRGB8 $ generateImage f (maxX - minX + 1) (maxY - minY + 1)
+    f x y = toColor $ Map.findWithDefault defaultValue (V2 (x + minX) (y + minY)) mapa
+    coords = Map.keysSet mapa
+    coordsx = Set.map (^. _x) coords
+    coordsy = Set.map (^. _y) coords
+    [minX, maxX] = map ($ coordsx) [Set.findMin, Set.findMax]
+    [minY, maxY] = map ($ coordsy) [Set.findMin, Set.findMax]
+
+susedi :: V2 Int -> [V2 Int]
+susedi coord = map (+ coord) [V2 0 1, V2 1 0, V2 (-1) 0, V2 0 (-1)]
+
+generateGraph :: Map (V2 Int) Int -> (Gr Int Int, Map (V2 Int) Int)
+generateGraph mapa = (mkGraph nodes edges, indexMap)
+  where
+    coords = Set.toList $ Map.keysSet mapa
+    indexMap = Map.fromList $ zip coords [1 ..]
+    nodes = zipWith (\coord index -> (index, mapa Map.! coord)) coords [1 ..]
+    edges = concatMap f coords
+    f coord = mapMaybe g (susedi coord)
+      where
+        g sused = do
+          tip <- Map.lookup sused mapa
+          guard (tip > 0)
+          return (indexMap Map.! coord, indexMap Map.! sused, 1)
+
+emptyLine :: String -> Bool
+emptyLine = all isSpace
